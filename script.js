84f60f048d500a376bec25d4acd3aca917af29d4
akshat.upadhyay <akshat.upadhyay@outlook.com>
2022-06-27 23:38:56 +0530

    Implement player switch and hold

--- script.js
+++ script.js
@@ -9,13 +9,61 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0Section = document.querySelector('.player--0');
+const player1Section = document.querySelector('.player--1');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-let currentScore = 0;
+// 0 for Player 1, 1 for Player 2
+let activePlayer = 0;
+
+// Create player class
+function Player(
+  score,
+  currentScore,
+  scoreElement,
+  currentScoreElement,
+  sectionElement
+) {
+  this.score = score;
+  this.currentScore = currentScore;
+  this.scoreElement = scoreElement;
+  this.currentScoreElement = currentScoreElement;
+  this.sectionElement = sectionElement;
+
+  this.setCurrentScoreElement = function (newScore, increment) {
+    this.currentScore = increment ? this.currentScore + newScore : newScore;
+    this.currentScoreElement.textContent = this.currentScore;
+  };
+
+  this.addCurrentScoreToScore = function () {
+    this.score += this.currentScore;
+    this.scoreElement.textContent = this.score;
+  };
+
+  this.makeActive = function () {
+    this.sectionElement.classList.add('player--active');
+  };
+
+  this.makeInactive = function () {
+    this.sectionElement.classList.remove('player--active');
+  };
+}
+
+const player0 = new Player(0, 0, score0El, currentScore0El, player0Section);
+const player1 = new Player(0, 0, score1El, currentScore1El, player1Section);
+
+const players = [player0, player1];
+
+const switchPlayer = function () {
+  players[activePlayer].setCurrentScoreElement(0, false);
+  players[activePlayer].makeInactive();
+  activePlayer = 1 - activePlayer;
+  players[activePlayer].makeActive();
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -30,10 +78,15 @@ btnRoll.addEventListener('click', function () {
   // Check if rolled 1:
   if (dice != 1) {
     // Add dice to current score
-    currentScore += dice;
-    currentScore0El.textContent = currentScore; // CHANGE LATER (Both players)
+    players[activePlayer].setCurrentScoreElement(dice, true);
 
     // Switch to next player
   } else {
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  players[activePlayer].addCurrentScoreToScore();
+  switchPlayer();
 });
